cd8dc680ab154e854485d3f15ff17d6c4a6166cb
dvforest <dvforest@hotmail.com>
2026-09-10 18:39:33 -0400

    Add nav bar links and cart icon

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,16 +1,24 @@
 import { AdvancedImage } from '@cloudinary/react';
 import cld from '../../utils/cloudinary';
 import styles from './Nav.module.css';
+import { ShoppingCart } from 'lucide-react';
 
 function Nav() {
   return (
     <div className={styles.navBar}>
-      <div className={styles.logoContainer}>
+      <div className={styles.logoSection}>
         <AdvancedImage
           cldImg={cld.image('logo-immersal')}
           className={styles.logoVector}
         />
         <div className={styles.logoText}>IMMERSAL</div>
+      </div>
+      <div className={styles.linkSection}>
+        <div className={styles.navText}>Home</div>
+        <div className={styles.navText}>Shop</div>
+        <div className="cartContainer">
+          <ShoppingCart size={30} />
+        </div>
       </div>
     </div>
   );
