1e1bd6fe4418f892d3e5b788ddf70a30fec9ba66
dvforest <dvforest@hotmail.com>
2026-09-15 13:06:26 -0400

    Add items in cart

--- src/components/CartModal/CartModal.jsx
+++ src/components/CartModal/CartModal.jsx
@@ -1,20 +1,55 @@
 import styles from './CartModal.module.css';
 import { X } from 'lucide-react';
+import { Trash } from 'lucide-react';
+import cld from '../../utils/cloudinary';
+import { AdvancedImage } from '@cloudinary/react';
+import Counter from '../Counter/Counter.jsx';
+import { books } from '../../data/books.js';
 
 function CartModal({ isOpen, onClose, inCart }) {
   const cartTotal = Object.values(inCart).reduce((sum, val) => sum + val, 0);
-
   return (
     <div className={styles.modalContainer}>
       <div className={`${styles.modal} ${isOpen ? styles.modalVisible : ''}`}>
-        <h2 className={styles.header}>Shopping Cart</h2>
-        <div className={styles.totalItems}>Contains {cartTotal} items.</div>
+        <h2 className={styles.header}>Your Cart</h2>
+        <div className={styles.totalItems}>
+          {cartTotal} item{cartTotal > 1 && 's'} in your cart.
+        </div>
         <X
           className={`${styles.x} ${isOpen ? styles.xVisible : ''}`}
           onClick={onClose}
           size={30}
           strokeWidth={2}
         />
+        {Object.entries(inCart).map(([id, item]) => {
+          const i = books.findIndex((book) => book.id === id);
+          return (
+            <div
+              className={styles.itemContainer}
+              key={id}
+            >
+              <div className={styles.iconSection}>
+                <AdvancedImage
+                  cldImg={cld.image('book-icon')}
+                  className={styles.bookIcon}
+                />
+                <div className={styles.titleContainer}>
+                  <div className={styles.keyText}>{books[i].genre}</div>
+                  <div className={styles.subText}>{books[i].binding}</div>
+                </div>
+              </div>
+              <div className={styles.countSection}>
+                <Counter />
+                <div className={styles.row}>
+                  <div className={styles.price}>$ {books[i].price}</div>
+                  <button className={styles.trashIcon}>
+                    <Trash />
+                  </button>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
       <div className={`${styles.dark} ${isOpen ? styles.darkVisible : ''}`} />
     </div>
