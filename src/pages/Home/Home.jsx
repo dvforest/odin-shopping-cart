a13db0d451c4c5c2023e24f815943a4d53008c63
dvforest <dvforest@hotmail.com>
2026-09-19 08:00:26 -0400

    Fix white padding appearing on mobile

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -17,7 +17,7 @@ function Home() {
         <h1>ENDLESS WONDER.</h1>
         <p>
           Not sure what fantasy book to read next? At Immersal, we're here to
-          help you re-kindle the simple joy of book shopping — by embracing
+          help you re-kindle the simple joy of book shopping... by embracing
           surprises.
         </p>
         <NavLink
