af8df1e977066e8a7516c829bdbb2e58db15deb9
dvforest <dvforest@hotmail.com>
2026-09-24 19:25:34 -0400

    Adjust layout to fit on mobile without scrolling

--- src/components/BookInfo/BookInfo.jsx
+++ src/components/BookInfo/BookInfo.jsx
@@ -7,13 +7,15 @@ function BookInfo({ book, count, onPlus, onMinus, onAddCart }) {
       <h2 className={styles.genre}>{book.genre}</h2>
       <div className={styles.binding}>{book.binding}</div>
       <p className={styles.description}>{book.description}</p>
-      <div className={styles.price}>{'$' + book.price}</div>
-      <div className={styles.counter}>
-        <Counter
-          count={count}
-          onPlus={onPlus}
-          onMinus={onMinus}
-        />
+      <div className={styles.priceAndCounterContainer}>
+        <div className={styles.price}>{'$' + book.price}</div>
+        <div className={styles.counter}>
+          <Counter
+            count={count}
+            onPlus={onPlus}
+            onMinus={onMinus}
+          />
+        </div>
       </div>
       <button
         className={styles.addButton}
